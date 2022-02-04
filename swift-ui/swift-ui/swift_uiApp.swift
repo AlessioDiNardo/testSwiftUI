8009683a4692b28742ed729da832b92a1f464bb9
Alessio Di Nardo <alessio.dinardo91@gmail.com>
2022-02-04 15:06:01 +0100

    first commit

---
+++ swift-ui/swift-ui/swift_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_uiApp.swift
+//  swift-ui
+//
+//  Created by Alessio Di Nardo on 04/02/22.
+//
+
+import SwiftUI
+
+@main
+struct swift_uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
